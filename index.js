63971127048683710e4a08b2b7ef102d68b30428
Armengod Sorrosal, D <d.armengod.sorrosal@accenture.com>
2019-06-20 17:24:54 +0200

    subida de robot

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 
-async function lsExample() {
-  const { stdout, stderr } = await exec('dir');
-  console.log('stdout:', stdout);
-  console.log('stderr:', stderr);
+
+async function execCommand(command){
+  const { stdout, stderr } = await exec(command);
+  return stdout != null ? stdout : stderr != null ? stderr : null;
 }
-lsExample();
+
+async function init() {
+  var command = await execCommand('git status');
+  console.log(command);
+
+  if( command.includes('modified') ){
+    command = await execCommand('git add .');
+    console.log(command);
+    command = await execCommand('git commit -m "subida de robot"');
+    console.log(command);
+    command = await execCommand('git push');
+  }
+  console.log( 'COMANDO FINAL :: ' +  command );
+}
+
+
+
+init();
